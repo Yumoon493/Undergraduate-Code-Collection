32104dfbdb635b5172973de182607325fed19a3f
Starfruit <2490151535@qq.com>
2026-01-17 05:00:05 +0800

    Add files via upload

---
+++ C-code/standard1.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+void selectionSort(vector<int>& A) {
+    for (int i = 0; i < A.size() - 1; i++) {
+        int min_index = i;
+        for (int j = i + 1; j < A.size(); j++) {
+            if (A[j] < A[min_index]) {
+                min_index = j;
+            }
+        }
+        swap(A[i], A[min_index]);   // Exchange elements
+    }
+}
+
+int main() {
+    vector<int> A = { 11, 22, 14, 67, 2, 9 };
+    selectionSort(A);
+    cout << "Sorted array: ";
+    for (int i = 0; i < A.size(); i++) {
+        cout << A[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
